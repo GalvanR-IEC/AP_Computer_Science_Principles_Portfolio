51d33edf68176a2fe438fab4f67fe089dedcd5aa
GalvanR-IEC <Royceg@nycstudents.net>
2025-12-04 18:20:20 -0500

    Added sparkle effect to the teeth.

--- GalvanR_Emoji_Task/GalvanR_Emoji_Task_Code.py
+++ GalvanR_Emoji_Task/GalvanR_Emoji_Task_Code.py
@@ -1,5 +1,5 @@
 #background
-Rect(0,0,400,400, fill='skyblue')
+Rect(0,0,400,400, fill=gradient('skyblue','deepskyblue',start='top'))
 #face
 Circle(200 ,200 ,150, fill='yellow' , border='black' , borderWidth=4)
 #eyes
@@ -8,4 +8,7 @@ Circle(250 ,150 ,20, fill='black')
 Circle(145,145,8, fill='white')
 Circle(245,145,8, fill='white')
 #smile
-Arc(200, 250, 100, 50, 90, 180,  border='black', borderWidth=4)
+Arc(200, 250, 100, 50, 90, 180,  border='white', borderWidth=4)
+Arc(200, 250, 100, 50, 90, 180,  border='white', borderWidth=4)
+#sparkle
+Star(240,260,18,5, fill='white', border='gold', borderWidth=2, roundness=20)
